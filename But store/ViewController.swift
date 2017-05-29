94438a1bdbd6385d9bc0173ff0590beda43c25f5
DoMico <domico@DoMicos-Mac.local>
2017-05-29 01:56:50 -0700

    Initial Commit

---
+++ But store/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  But store
+//
+//  Created by DoMico on 5/28/17.
+//  Copyright © 2017 DoMico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
